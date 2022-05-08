a07ca3777fcd729a9145ec61428ab907e69cedbc
monicacosta-dev <monicacosta07@gmail.com>
2022-05-08 14:50:03 -0300

    atualização html css js

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() { //nao esquecer do evento onclick
+    const textoEncriptado = encrip(".inputTexto.value")
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImge = "none"
+
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [
+        ["a", "ai"],
+        ["e", "enter"],
+        ["i", "imes"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0]), (matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+
+}
