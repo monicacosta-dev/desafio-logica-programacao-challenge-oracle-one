8c2a0c762311c6b79f8bdae41d80d05ecb7a94e3
monicacosta-dev <monicacosta07@gmail.com>
2022-05-11 22:05:49 -0300

    concluindo a criptografia e a sumida do tesouro.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const mensagem = document.querySelector(".mensagem");
 function btnEncriptar() { //nao esquecer do evento onclick
     const textoEncriptado = encriptar(inputTexto.value)
     mensagem.value = textoEncriptado
-    mensagem.style.backgroundImge = "none"
+    mensagem.style.background = "none"
 
 }
 
@@ -16,11 +16,13 @@ function encriptar(stringEncriptada) {
         ["o", "ober"],
         ["u", "ufat"]
     ];
+
     stringEncriptada = stringEncriptada.toLowerCase();
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0]), (matrizCodigo[i][1])
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
         }
+
     }
 
     return stringEncriptada;
