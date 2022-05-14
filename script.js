8c672013f95622bf42c9b6a18e5f7d64dfe94b4f
monicacosta-dev <monicacosta07@gmail.com>
2022-05-14 14:00:02 -0300

    atualização html css

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ function encriptar(stringEncriptada) {
     return stringEncriptada;
 
 }
+
+function btnDescriptar() { //nao esquecer do evento onclick
+    const textoDescriptado = descriptar(inputTexto.value)
+    mensagem.value = textoDescriptado
+        //mensagem.style.background = "none"
+
+}
+
+function descriptar(stringDescriptada) {
+    let matrizCodigo = [
+        ["a", "ai"],
+        ["e", "enter"],
+        ["i", "imes"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+
+    stringDescriptada = stringDescriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDescriptada.includes(matrizCodigo[i][0])) {
+            stringDescriptada = stringDescriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+
+    }
+
+    return stringDescriptada;
+
+}
