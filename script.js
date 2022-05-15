98cde94cf0b12107c7e2aa66bef9ca470a5a4d82
monicacosta-dev <monicacosta07@gmail.com>
2022-05-15 18:52:40 -0300

    Atualização de Entrega do projeto!

--- script.js
+++ script.js
@@ -1,79 +1,71 @@
 const inputTexto = document.querySelector(".input-texto");
 const mensagem = document.querySelector(".mensagem");
 
-function btnEncriptar() { //nao esquecer do evento onclick
-    const textoEncriptado = encriptar(inputTexto.value)
-    mensagem.value = textoEncriptado
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado;
     mensagem.style.backgroundImage = "none";
-    inputTexto.value = "";
-
 }
 
 function encriptar(stringEncriptada) {
-    let matrizCodigo = [
-        ["a", "ai"],
+    let codificar = [
         ["e", "enter"],
         ["i", "imes"],
+        ["a", "ai"],
         ["o", "ober"],
         ["u", "ufat"]
     ];
-
     stringEncriptada = stringEncriptada.toLowerCase();
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+
+
+    for (let i = 0; i < codificar.length; i++) {
+        if (stringEncriptada.includes(codificar[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(codificar[i][0], codificar[i][1]);
         }
-
     }
-
     return stringEncriptada;
-
 }
 
-function btnDescriptar() { //nao esquecer do evento onclick
-    const textoDescriptado = descriptar(inputTexto.value)
-    inputTexto.value = textoDescriptado;
-    mensagem.style.backgroundImage = "";
-    inputTexto.value = "";
-
+function btnDesencriptar() {
+    const textoDescriptado = descriptar(inputTexto.value);
+    mensagem.value = textoDescriptado;
+    mensagem.style.backgroundImage = "none";
 }
 
 function descriptar(stringDescriptada) {
-    let matrizCodigo = [
-        ["a", "ai"],
-        ["e", "enter"],
-        ["i", "imes"],
-        ["o", "ober"],
-        ["u", "ufat"]
+    let descodificar = [
+        ["ai", "a"],
+        ["enter", "e"],
+        ["imes", "i"],
+        ["ober", "o"],
+        ["ufat", "u"]
     ];
 
-    stringDescriptada = stringDescriptada.toLowerCase();
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDescriptada.includes(matrizCodigo[i][0])) {
-            stringDescriptada = stringDescriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    for (let i = 0; i < descodificar.length; i++) {
+        if (stringDescriptada.includes(descodificar[i][0])) {
+            stringDescriptada = stringDescriptada.replaceAll(descodificar[i][0], descodificar[i][1]);
         }
-
     }
-
     return stringDescriptada;
-
 }
 
 function btncopiar() {
-    let copiarTexto = document.querySelector(".mensagem");
-    copiarTexto.select();
-    document.execCommand("copy");
-    mensagem.value = "";
-    btnCopiar.style.display = "none";
-    mensagem.style.display = "none";
-    alert("Texto copiado para área de transferência");
+    const textoCopiado = document.querySelector(".mensagem");
+    textoCopiado.select();
+    navigator.clipboard.writeText(textoCopiado.value);
+    alert("Texto copiado,  cole ele na aréa de transferencia!");
+    window.location.reload();
+
 
 }
 
 function btnLimpar() {
+    const textlimpado = document.querySelector(".mensagem");
     mensagem.value = "";
     inputTexto.value = "";
-    document.getElementById("#limpar").style.display = "block";
+    document.getElementById("mensagem").style.display = "block";
     inputTexto.value = "";
     inputTexto.focus();
+    mensagem.style.backgroundImage = "";
 }
